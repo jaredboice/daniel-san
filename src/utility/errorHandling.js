ee3425588f167e22dfa7ee1c45b85f3c527e789f
Jared Boice2 <jaredboice2@Jareds-iMac.local>
2019-06-11 08:34:16 -0400

    updated default error message

--- src/utility/errorHandling.js
+++ src/utility/errorHandling.js
@@ -1,4 +1,4 @@
-const errorDisc = (error = {}, message = '', moreInfo = {}) => {
+const errorDisc = (error = {}, message = 'error: something bad happened and a lot of robots died', moreInfo = {}) => {
     const err = error || {};
     return {
         err,
